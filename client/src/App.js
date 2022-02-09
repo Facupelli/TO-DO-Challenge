31ac10a06f22b26ef70c306bcb2b44740dad8fd5
Facupelli <facundopellicer4@gmail.com>
2022-02-09 15:53:21 -0300

    styles, darkMode

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
+import { useState } from "react";
 import { Home } from "./components/Home";
 
+
 function App() {
+  const [darkMode, setDarkMode] = useState(false)
+
   return (
-    <div className="flex justify-center min-h-screen">
-      <Home />
+    <div className={`${darkMode ? 'bg-black' : ''} flex justify-center min-h-screen`}>
+      <Home setDarkMode={setDarkMode} darkMode={darkMode} />
     </div>
   );
 }
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -2,14 +2,15 @@ import React, { useEffect, useState } from "react";
 import { Folders } from "./Folders";
 import { Todos } from "./Todos";
 import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
 
-export const Home = () => {
+export const Home = ({ darkMode, setDarkMode }) => {
   const [folders, setFolders] = useState([]);
   const [folderId, setFolderId] = useState(null);
 
   const [showTodos, setShowTodos] = useState(false);
   const [todos, setTodos] = useState([]);
-
 
   useEffect(() => {
     axios.get("/folder").then((res) => setFolders(res.data));
@@ -21,12 +22,23 @@ export const Home = () => {
     setFolderId(id);
   };
 
+  const handleDarkMode = () => {
+    setDarkMode(!darkMode)
+  }
+
   return (
     <div className="w-full pt-10 mx-20 ">
+      <div onClick={handleDarkMode} className="flex justify-end cursor-pointer ">
+        {darkMode ? (
+          <FontAwesomeIcon icon={faSun} size='xl' className="text-main hover:text-secondary"/>
+        ) : (
+          <FontAwesomeIcon icon={faMoon} size='xl' className="text-secondary hover:text-main"/>
+        )}
+      </div>
       <div className="mb-10 flex justify-center">
         <div>
-          <p className="font-bold text-5xl font-title ">TODO APP</p>
-          <p className="text-gray-500 font-title">by Facupelli</p>
+          <p className={`font-bold text-5xl font-title ${darkMode? 'text-mainLight':''}`}>TODO APP</p>
+          <p className="text-secondaryLight font-title">by Facupelli</p>
         </div>
       </div>
       <div className="grid grid-cols-2 gap-6 w-full font-body">
@@ -41,11 +53,7 @@ export const Home = () => {
         </div>
         <div className="col-span-1 p-4 bg-secondaryLight rounded">
           {showTodos && (
-            <Todos
-              todos={todos}
-              setTodos={setTodos}
-              folderId={folderId}
-            />
+            <Todos todos={todos} setTodos={setTodos} folderId={folderId} />
           )}
         </div>
       </div>
